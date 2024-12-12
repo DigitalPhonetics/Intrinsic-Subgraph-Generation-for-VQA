8f6a93a404b8f6247b5e7d75514b28cde5426420
Pascal Tilli <pascal.tilli@ims.uni-suttgart.de>
2024-12-12 11:44:02 +0100

    update rtd

--- docs/conf.py
+++ docs/conf.py
@@ -5,23 +5,26 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
 
-project = 'Intrinsic Subgraph Generation for interpretable VQA'
-copyright = '2024, Pascal Tilli'
-author = 'Pascal Tilli'
+sys.path.insert(0, os.path.abspath("../ISubGVQA"))
+
+project = "Intrinsic Subgraph Generation for interpretable VQA"
+copyright = "2024, Pascal Tilli"
+author = "Pascal Tilli"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
